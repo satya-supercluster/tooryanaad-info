72d4bd219fca0badaeec5a5e27826911484d1ae1
satya-supercluster <satya.supercluster@gmail.com>
2024-09-02 22:36:59 +0530

    animation in login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../data/AuthContext";
 import AnimatedDiv from "./AnimatedDiv";
+import { motion, AnimatePresence } from "framer-motion";
 const Login = () => {
   const { passcode, setPasscode } = useAuth();
   const handlePasscodeChange = (event) => {
@@ -9,13 +10,42 @@ const Login = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-700">
       <AnimatedDiv className=" bg-black bg-opacity-30 shadow-lg rounded-lg p-6 min-h-[50vh] m-2 flex flex-col justify-center items-center">
-        <h1 className="text-3xl text-center font-extrabold mb-4 text-blue-200">
+        <h1 className="text-3xl text-center font-extrabold my-5 sm:my-10 text-blue-200">
           LOGIN
         </h1>
-        <img src="/imgLogoXL.png" alt="TY" className="w-1/5 max-md:w-1/3" />
-        <h1 className="text-xl max-md:text-lg text-center font-bold mb-4 text-green-100">
+        <img
+          src="/imgLogoXL.png"
+          alt="TY"
+          className="animate-bounce max-w-[100px] lg:max-w-[150px]"
+        />
+        <h1 className="text-xl max-md:text-lg text-center font-bold my-2 text-green-100">
           Tooryanaad Samiti Events Information
         </h1>
+        <AnimatePresence mode="wait">
+          {passcode === "" ? (
+            <motion.div
+              key="empty"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 50 }}
+              transition={{ duration: 0.2 }}
+              className="text-blue-500 p-2 text-sm lg:text-lg font-semibold my-1 text-center"
+            >
+              Passcode Avashyak!
+            </motion.div>
+          ) : (
+            <motion.div
+              key="incorrect"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -50 }}
+              transition={{ duration: 0.2 }}
+              className="text-red-500 p-2 text-sm lg:text-lg font-semibold my-1 text-center"
+            >
+              PassCode Galat hai
+            </motion.div>
+          )}
+        </AnimatePresence>
         <input
           type="password"
           value={passcode}
@@ -23,11 +53,6 @@ const Login = () => {
           placeholder="Enter passcode"
           className="w-full max-w-52 px-4 py-2 border-2 rounded-md border-yellow-500 focus:outline-none focus:border-blue-500 font-bold text-slate-700 text-center"
         />
-        {passcode === "" ? null : (
-          <div className="text-red-500 p-2 text-sm lg:text-lg font-semibold mt-2 text-center">
-            PassCode Galat hai
-          </div>
-        )}
       </AnimatedDiv>
     </div>
   );
